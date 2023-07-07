9184cded44a04f6122aa80091e6bc8d49e7e9aab
Aniruddh Pandya <aniruddh.p@peopleinteractive.in>
2023-07-08 00:57:06 +0530

    sr indicator tuning

--- indicators/__init__.py
+++ indicators/__init__.py
@@ -1,1 +1,2 @@
 from .cb_indicator import ChannelBreakoutIndicator
+from .sr_indicator import SupportResistanceIndicator
--- indicators/sr_indicator.py
+++ indicators/sr_indicator.py
@@ -1,131 +1,136 @@
 import numpy as np
+import pandas as pd
+import pandas_ta as ta
 import plotly.graph_objects as go
-from scipy import stats
 
 class SupportResistanceIndicator:
 
-    def __init__(self, data):
-        self.window = 4
+    def __init__(self, data, window, backCandles):
+        self.window = window
+        self.backCandles = backCandles
         self.df = data
-        self.wickThreshold = 0.001
-        
+        self.df['RSI'] = ta.rsi(data.Close, length=14)
 
-    def support(self, candleIndex, candlesBefore, candlesAfter):
-        if ( self.df.low[candleIndex-candlesBefore:candleIndex].min() < self.df.low[candleIndex] or
-            self.df.low[candleIndex+1:candleIndex+candlesAfter+1].min() < self.df.low[candleIndex] ):
-            return 0
+        # candle proximity with a level
+        self.proximity = (((data.High.mean())-data.Low.mean())/data.High.mean()) * 50
+        # proximity b/w levels
+        self.levelProximity = max(data.High)/100
 
-        candle_body = abs(self.df.open[candleIndex]-self.df.close[candleIndex])
-        lower_wick = min(self.df.open[candleIndex], self.df.close[candleIndex])-self.df.low[candleIndex]
-        if (lower_wick > candle_body) and (lower_wick > self.wickThreshold): 
-            return 1
-        
-        return 0
-    
 
-    def resistance(self, candleIndex, candlesBefore, candlesAfter):
-        if ( self.df.high[candleIndex-candlesBefore:candleIndex].max() > self.df.high[candleIndex] or
-            self.df.high[candleIndex+1:candleIndex+candlesAfter+1].max() > self.df.high[candleIndex] ):
-            return 0
-        
-        candle_body = abs(self.df.open[candleIndex]-self.df.close[candleIndex])
-        upper_wick = self.df.high[candleIndex]-max(self.df.open[candleIndex], self.df.close[candleIndex])
-        if (upper_wick > candle_body) and (upper_wick > self.wickThreshold) :
-            return 1
+    def getLevels(self):
+        dfSlice = self.df[-252:]
+        supports = dfSlice[dfSlice.Low == dfSlice.Low.rolling(self.window, center=True).min()].Low
+        resistances = dfSlice[dfSlice.High == dfSlice.High.rolling(self.window, center=True).max()].High
+        levels = pd.concat([supports, resistances])
+        return levels[abs(levels.diff()) > self.levelProximity]
 
-        return 0
-    
 
-    def closeResistance(self, l,levels,lim, df):
+    def isCloseToResistance(self, candleIndex, levels):
         if len(levels)==0:
             return 0
-        minLevel = min(levels, key=lambda x:abs(x-df.high[l]))
-        c1 = abs(df.high[l]-minLevel)<=lim
-        c2 = abs(max(df.open[l],df.close[l])-minLevel)<=lim
-        c3 = min(df.open[l],df.close[l])<minLevel
-        c4 = df.low[l]<minLevel
+        minLevel = min(levels, key=lambda x:abs(x-self.df.High[candleIndex]))
+        c1 = abs(self.df.High[candleIndex]-minLevel)<=self.proximity
+        c2 = abs(max(self.df.Open[candleIndex],self.df.Close[candleIndex])-minLevel)<=self.proximity
+        c3 = min(self.df.Open[candleIndex],self.df.Close[candleIndex])<minLevel
+        c4 = self.df.Low[candleIndex]<minLevel
         if( (c1 or c2) and c3 and c4 ):
             return minLevel
         else:
             return 0
     
     
-    def closeSupport(self, l,levels,lim, df):
+    def isCloseToSupport(self, candleIndex, levels):
         if len(levels)==0:
             return 0
-        minLevel = min(levels, key=lambda x:abs(x-df.low[l]))
-        c1 = abs(df.low[l]-minLevel)<=lim
-        c2 = abs(min(df.open[l],df.close[l])-minLevel)<=lim
-        c3 = max(df.open[l],df.close[l])>minLevel
-        c4 = df.high[l]>minLevel
+        minLevel = min(levels, key=lambda x:abs(x-self.df.Low[candleIndex]))
+        c1 = abs(self.df.Low[candleIndex]-minLevel)<=self.proximity
+        c2 = abs(min(self.df.Open[candleIndex],self.df.Close[candleIndex])-minLevel)<=self.proximity
+        c3 = max(self.df.Open[candleIndex],self.df.Close[candleIndex])>minLevel
+        c4 = self.df.High[candleIndex]>minLevel
         if( (c1 or c2) and c3 and c4 ):
             return minLevel
         else:
             return 0
         
 
-    def isBelowResistance(self, l, level_backCandles, level, df):
-        return df.loc[l-level_backCandles:l-1, 'high'].max() < level
+    def arePrevCandlesBelowResistance(self, candleIndex, level):
+        return self.df.loc[candleIndex-self.backCandles:candleIndex-1, 'High'].max() < level
 
 
-    def isAboveSupport(self, l, level_backCandles, level, df):
-        return df.loc[l-level_backCandles:l-1, 'low'].min() > level  
+    def arePrevCandlesAboveSupport(self, candleIndex, level):
+        return self.df.loc[candleIndex-self.backCandles:candleIndex-1, 'Low'].min() > level  
 
 
-    def check_candle_signal(self, l, n1, n2, backCandles, df):
-        ss = []
-        rr = []
-        for subrow in range(l-backCandles, l-n2):
-            if self.support(df, subrow, n1, n2):
-                ss.append(df.low[subrow])
-            if self.resistance(df, subrow, n1, n2):
-                rr.append(df.high[subrow])
-        
-        ss.sort() #keep lowest support when popping a level
-        for i in range(1,len(ss)):
-            if(i>=len(ss)):
-                break
-            if abs(ss[i]-ss[i-1])<=0.0001: # merging close distance levels
-                ss.pop(i)
+    def getCandleSignal(self, candleIndex):
+        levels = self.getLevels()
 
-        rr.sort(reverse=True) # keep highest resistance when popping one
-        for i in range(1,len(rr)):
-            if(i>=len(rr)):
-                break
-            if abs(rr[i]-rr[i-1])<=0.0001: # merging close distance levels
-                rr.pop(i)
+        cR = self.isCloseToResistance(candleIndex, levels)
+        cS = self.isCloseToSupport(candleIndex, levels)
 
-        #----------------------------------------------------------------------
-        # joined levels
-        rrss = rr+ss
-        rrss.sort()
-        for i in range(1,len(rrss)):
-            if(i>=len(rrss)):
-                break
-            if abs(rrss[i]-rrss[i-1])<=0.0001: # merging close distance levels
-                rrss.pop(i)
-        cR = self.closeResistance(l, rrss, 150e-5, df)
-        cS = self.closeSupport(l, rrss, 150e-5, df)
-        #----------------------------------------------------------------------
-
-        # cR = closeResistance(l, rr, 150e-5, df)
-        # cS = closeSupport(l, ss, 150e-5, df)
-        # could we consider the average RSI for the trend momentum?
-        if (cR and self.isBelowResistance(l,6,cR, df) and df.RSI[l-1:l].min()<45 ):#and df.RSI[l]>65
+        if (cR and self.arePrevCandlesBelowResistance(candleIndex, cR) and self.df.RSI[candleIndex-1:candleIndex].min()<45 ):#and df.RSI[l]>65
             return 1
-        elif(cS and self.isAboveSupport(l,6,cS,df) and df.RSI[l-1:l].max()>55 ):#and df.RSI[l]<35
+        elif(cS and self.arePrevCandlesAboveSupport(candleIndex, cS) and self.df.RSI[candleIndex-1:candleIndex].max()>55 ):#and df.RSI[l]<35
             return 2
         else:
             return 0
 
 
-    def setSignal(self, backCandles=40):
-        self.df["Signal"] = self.getSignal(backCandles)
-
-
-    def getSignal(self, backCandles=40):
+    def showIndicator(self, candles):
+        dfSlice = self.df[-candles:]
+        fig = go.Figure(data=[go.Candlestick(x=dfSlice.index,
+                        open=dfSlice["Open"],
+                        high=dfSlice["High"],
+                        low=dfSlice["Low"],
+                        close=dfSlice["Close"])])
         
-        self.setPivotPoint()
-        self.setBreakoutPoint(backCandles)
 
-        self.df.isBreakout
+        levels = self.getLevels()
+        levels = levels[levels > (min(dfSlice.Close) - self.levelProximity)]
+        levels = levels[levels < (max(dfSlice.High) + self.levelProximity)]
+
+        for level in levels.to_list():
+            fig.add_shape(
+                type='line',
+                x0=dfSlice.index.start - 2,
+                y0=level,
+                x1=dfSlice.index.stop + 2,
+                y1=level,
+                line=dict(color='blue'),
+                xref='x',
+                yref='y',
+                layer='below'
+            )
+
+        fig.add_scatter(x=dfSlice.index, y=dfSlice["SignalMarker"], mode="markers",
+                        marker=dict(size=7, color="Black"), marker_symbol="hexagram",
+                        name="signal")
+
+        fig.show()
+        
+    
+    def setSignalMarker(self):
+        self.df["SignalMarker"] = [self.getSignalMarker(row) for index, row in self.df.iterrows()]
+
+
+    def getSignalMarker(self, x):
+        markerDistance = (x["High"]-x["Low"])/10
+        if x["Signal"]==2:
+            return x["Low"]-markerDistance
+        elif x["Signal"]==1:
+            return x["High"]+markerDistance
+        else:
+            return np.nan
+        
+    
+    def calculate(self):
+        self.setSignal()
+        self.setSignalMarker()
+
+
+    def setSignal(self):
+        self.df["Signal"] = self.getSignal()
+
+
+    def getSignal(self):
+        return [self.getCandleSignal(candle) for candle in self.df.index]
+        
