574796c003e042cc69803b058f5f2ef968148427
Aniruddh Pandya <aniruddh.p@peopleinteractive.in>
2023-08-08 00:20:38 +0530

    test

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ data.set_index('Date').reset_index()
 data = data.drop(['Date'], axis=1)
 data = data[['Open', 'High', 'Low', 'Close', 'Volume']]
 st.dataframe(data)
+
+from datetime import date
+from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save
+
+# Download bhavcopy
+path = bhavcopy_save(date(2020,1,1), "./database")
+st.write(path)
+df = pd.read_csv(path)
+st.dataframe(df)
